61d0ff489a228a7705d5ded7027264c2075eb184
JiaYing-00 <jiaying101200@gmail.com>
2022-08-10 09:59:18 +0800

    Modified the label1 to Hello first modified code.

--- Git Test/Form1.cs
+++ Git Test/Form1.cs
@@ -16,5 +16,10 @@ namespace Git_Test
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hello first modified code";
+        }
     }
 }
